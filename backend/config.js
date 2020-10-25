9814b18296cea5af789fe200a7a3d26926e993eb
Phi Nguyen <phinguyenphuoc@gmail.com>
2020-10-25 14:59:41 +0700

    Init sequelize

---
+++ backend/config.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+})
+
+module.exports = {
+  query: (text, params, callback) => {
+    const start = Date.now()
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start
+      console.log('executed query', { text, params, duration, rows: res.rowCount })
+      callback(err, res)
+    })
+  }
+}
