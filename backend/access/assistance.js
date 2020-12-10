769b79351165b2e6d7dd289b87d6deee1fbb6fe7
Phi Nguyen <phinguyenphuoc@gmail.com>
2020-12-10 23:18:47 +0700

    Fix assistance

--- backend/access/assistance.js
+++ backend/access/assistance.js
@@ -1,12 +1,13 @@
 const { query } = require('../config')
 
-const postAssistance = ({ profile_id, message }) => {
+const postAssistance = (profile_id, message) => {
   return new Promise((resolve, reject) => {
     query(
       `INSERT INTO public.assistance(profile_id, message) VALUES($1, $2) RETURNING *`,
       [profile_id, message],
       (error, results) => {
         if (error) {
+          console.log(error)
           reject(error)
         } else {
           resolve(results.rows[0])
